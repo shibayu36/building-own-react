2f61f8da2047ca4cd6f32a3aa57984901d217b18
shibayu36 <shibayu36@gmail.com>
2021-03-31 23:03:11 +0900

    Step 0

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+const element = {
+  type: "h1",
+  props: {
+    title: "foo",
+    children: "Hello",
+  },
+};
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const container = document.getElementById("root");
+if (!container) throw new Error("No root element");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const node = document.createElement(element.type);
+node["title"] = element.props.title;
+const text = document.createTextNode("");
+text["nodeValue"] = element.props.children;
+node.appendChild(text);
+container.appendChild(node);
+
+export {};
