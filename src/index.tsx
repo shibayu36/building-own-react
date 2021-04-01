8b5d14fb7584d8d1e8e9bec9262ea7933b8bb616
shibayu36 <shibayu36@gmail.com>
2021-04-01 22:40:49 +0900

    another examples

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,13 @@
-const element = {
-  type: "h1",
-  props: {
-    title: "foo",
-    children: "Hello",
-  },
-};
+import React from "react";
+import ReactDOM from "react-dom";
 
+const element = (
+  <div id="foo">
+    <a>bar</a>
+    <b />
+  </div>
+);
 const container = document.getElementById("root");
-if (!container) throw new Error("No root element");
-
-const node = document.createElement(element.type);
-node["title"] = element.props.title;
-const text = document.createTextNode("");
-text["nodeValue"] = element.props.children;
-node.appendChild(text);
-container.appendChild(node);
+ReactDOM.render(element, container);
 
 export {};
