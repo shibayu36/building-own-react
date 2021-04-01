db248958e9da072ffcadf3ac5df196276a560193
shibayu36 <shibayu36@gmail.com>
2021-04-01 22:58:45 +0900

    createElement

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,49 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const element = (
-  <div id="foo">
-    <a>bar</a>
-    <b />
-  </div>
+const Didact = {
+  createElement,
+};
+
+const element = Didact.createElement(
+  "div",
+  { id: "foo" },
+  Didact.createElement("a", null, "bar"),
+  Didact.createElement("b")
 );
-const container = document.getElementById("root");
-ReactDOM.render(element, container);
+console.log(element);
+// const container = document.getElementById("root");
+// ReactDOM.render(element, container);
+
+function createElement(type: string, props: DidactProps = {}, ...children: DidactChild[]): DidactElement {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) => (typeof child === "object" ? child : createTextElement(child))),
+    },
+  };
+}
+
+function createTextElement(text: DidactText): DidactElement {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
+
+type DidactElement = {
+  type: string;
+  props: DidactProps;
+};
+type DidactText = string | number;
+type DidactChild = DidactElement | DidactText;
+type DidactProps = {
+  children?: DidactElement[];
+  [key: string]: any;
+} | null;
 
 export {};
